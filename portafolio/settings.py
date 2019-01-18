c1726218f8410732f4f91c8b0781c4222ae21285
Damian Escobedo <damian050697@gmail.com>
2019-01-18 11:00:41 -0700

    Gets location of user

--- portafolio/settings.py
+++ portafolio/settings.py
@@ -114,7 +114,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-
+GEOIP_PATH = os.path.join(BASE_DIR, 'geoip')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
--- portafolio/urls.py
+++ portafolio/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('weatherapp/', include('weatherapp.urls')),
+    path('', include('weatherapp.urls')),
 ]
