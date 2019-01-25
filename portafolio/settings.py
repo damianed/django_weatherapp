36f178fd888c87d8bc5acff7897b0ea8a305a3d9
Damian Escobedo <damian050697@gmail.com>
2019-01-24 22:11:59 -0700

    GitHub and email links

--- portafolio/settings.py
+++ portafolio/settings.py
@@ -117,7 +117,6 @@ USE_TZ = True
 GEOIP_PATH = os.path.join(BASE_DIR, 'geoip')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-
 STATIC_URL = '/static/'
 
 # Configure Django App for Heroku.
